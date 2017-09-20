769d39e288f22098189ef3d38a323bfa488cc162
Bernal Gomez <d_bernalgomez@tss-hr420-dm07.fc.ca>
2017-09-20 16:10:59 -0400

    finishing feature branch

--- script/main.js
+++ script/main.js
@@ -1,1 +1,56 @@
+(function () {
+ var theImages = document.querySelectorAll('.image-holder'),
+     theHeader = document.querySelector('.heading'),
+     theSubhead = document.querySelector('.main-copy h2'),
+     theSeasonText = document.querySelector('.main-copy p'),
+     appliedClass;
 
+     function changeElements() {
+
+       // I want to to load dynamic content here
+       //debugger;
+       let subImages = document.querySelector('.subImagesContainer');
+       let objectIndex = dynamicContent[this.id];
+
+
+       //remove all the thumbnails
+       while (subImages.firstChild) {
+         subImages.removeChild(subImages.firstChild);
+       }
+
+       //
+
+       objectIndex.images.forEach(function(element, index){
+         let newSubImg=document.createElement('img');
+
+         // add a css class
+         newSubImg.classList.add('thumb');
+         // add an image source
+         newSubImg.src = "images/" + objectIndex.images[index];
+         // around it to the container
+         subImages.appendChild(newSubImg);
+       });
+
+      theSubhead.classList.remove(appliedClass);
+      theHeader.classList.remove(appliedClass);
+
+       theSubhead.classList.add(this.id);
+       theHeader.classList.add(this.id);
+
+       theSubhead.firstChild.nodeValue = objectIndex.headline;
+       theSeasonText.firstChild.nodeValue = objectIndex.text;
+
+       appliedClass = this.id;
+     }
+
+     theImages.forEach(function(element, index){
+       //loop through and do stuff to each element at the top of the page
+       element.addEventListener('click', changeElements, false);
+     });
+
+     //Initialize the appendChild
+     theSubhead.firstChild.nodeValue = dynamicContent['spring'].headline;
+     theSeasonText.firstChild.nodeValue = dynamicContent['spring'].text;
+     theHeader.classList.add('spring');
+
+})();
